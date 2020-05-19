0120ff6e8111d2385558c2b6d0e62792947720ad
kawsark <kawsar@hashicorp.com>
2020-05-19 02:52:28 -0400

    Added Dockerfile and steps

---
+++ scripts/deploy_app.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Script to deploy a very simple web application.
+# The web app has a customizable image and some text.
+
+# Set default values
+[[ -z $PREFIX ]] && PREFIX="Garfield"
+[[ -z $PLACEHOLDER ]] && PLACEHOLDER="placekitten.com"
+[[ -z $WIDTH ]] && WIDTH=400
+[[ -z $HEIGHT ]] && HEIGHT=800
+
+cat << EOM > /usr/local/apache2/htdocs/index.html
+<html>
+  <head><title>Meow!</title></head>
+  <body>
+  <div style="width:800px;margin: 0 auto">
+
+  <!-- BEGIN -->
+  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><h2>Meow World!</h2></center>
+  Welcome to ${PREFIX}'s Meowlicious app. 
+  <!-- END -->
+
+  </div>
+  </body>
+</html>
+EOM
+
+echo "Script complete."
